9d1d224dfcaf2d35254b71d9344da4ca2c9e50fd
Bohdan Kozar <203163016+engapantio@users.noreply.github.com>
2026-04-26 20:49:50 +0200

    Add --ignore-unfixed option to trivy scan
    Updated trivy command to include the --ignore-unfixed option.

--- vars/scanDockerImage.groovy
+++ vars/scanDockerImage.groovy
@@ -1,3 +1,3 @@
 def call(String repo, String imageTag) {
-  sh "trivy image --severity HIGH,CRITICAL --exit-code 1 ${repo}:${imageTag}"
+  sh "trivy image --severity HIGH,CRITICAL --ignore-unfixed --exit-code 1 ${repo}:${imageTag}"
 }
