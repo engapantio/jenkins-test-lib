e17dbcc1a74140640f02b1ad506b7827648a49e6
Bohdan Kozar <203163016+engapantio@users.noreply.github.com>
2026-04-26 18:57:23 +0200

    Simplify Docker image scanning function

--- vars/scanDockerImage.groovy
+++ vars/scanDockerImage.groovy
@@ -1,5 +1,3 @@
 def call(String repo, String imageTag) {
-  docker.image('aquasec/trivy:latest').inside('-v /var/run/docker.sock:/var/run/docker.sock') {
-    sh "trivy image --severity HIGH,CRITICAL --exit-code 1 ${repo}:${imageTag}"
-  }
+  sh "trivy image --severity HIGH,CRITICAL --exit-code 1 ${repo}:${imageTag}"
 }
