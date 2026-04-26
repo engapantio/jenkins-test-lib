3156b54017e4d2386c0e2040f71d614d8bf7f3d3
Bohdan Kozar <203163016+engapantio@users.noreply.github.com>
2026-04-26 19:45:27 +0200

    Fix image tag format in getImageTag function

--- vars/getImageTag.groovy
+++ vars/getImageTag.groovy
@@ -1,9 +1,9 @@
 def call(String branchName) {
   if (branchName == 'dev') {
-    return 'nodedev:v1.0'
+    return 'nodedev-v1.0'
   }
   if (branchName == 'main' || branchName == 'master') {
-    return 'nodemain:v1.0'
+    return 'nodemain-v1.0'
   }
   error("Unsupported branch: ${branchName}")
 }
